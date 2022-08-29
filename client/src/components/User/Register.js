77e56fad7a3224e565ea0daa4be6a4b101b73fac
seongho <seongho6022@naver.com>
2022-08-29 16:53:30 +0900

    login, register 진입 시 포커스 설정

--- client/src/components/User/Register.js
+++ client/src/components/User/Register.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import Form from "../Form";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+
 const StyeldForm = styled(Form)``;
 const Register = () => {
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
   const [confirm, setConfirm] = useState("");
+  const emailInput = useRef();
+  useEffect(() => {
+    emailInput.current.focus();
+  }, []);
+
   return (
     <StyeldForm
       className="registerForm"
@@ -14,6 +20,7 @@ const Register = () => {
         {
           label: "이메일",
           setStateFn: setEmail,
+          ref: emailInput,
         },
         {
           label: "비밀번호",
