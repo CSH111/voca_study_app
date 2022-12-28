7de810ffce729bf0540a6c3dc10122895843ee2b
CSH111 <seongho6022@naver.com>
2022-12-28 20:57:29 +0900

    feat: Bookmark 페이지 ui 적용

--- client/src/components/Bookmark/BookmarkList.js
+++ client/src/components/Bookmark/BookmarkList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
 import { DataContext } from "../../services/DataContext";
+import { Devider } from "../common";
 import { Spinner } from "../common/icons";
 import WordListItem from "../Words/WordListItem";
 
@@ -9,7 +10,12 @@ const BookmarkList = () => {
   const words = store.wordsData.words.filter((word) => word.isBookmarked === true);
   const isLoading = store.wordsData.loading;
   const listItem = words.length ? (
-    words.map((word) => <WordListItem wordID={word._id} key={word._id} />)
+    words.map((word, idx) => (
+      <>
+        <WordListItem wordID={word._id} key={word._id} />
+        {idx < words.length - 1 && <Devider margin="10px 0" width="2px" color="#c4c4c4" />}
+      </>
+    ))
   ) : (
     <div>단어를 추가하세요</div>
   );
--- client/src/pages/Bookmark.js
+++ client/src/pages/Bookmark.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 
 import { BookmarkList } from "../components/Bookmark";
+import { PageContainer, Paper, PaperTitle } from "../components/common";
 import Button from "../components/common/Button";
 import { GoBackIcon } from "../components/common/icons";
 
@@ -8,12 +9,25 @@ function Bookmark() {
   const navigate = useNavigate();
 
   return (
-    <>
-      <Button onClick={() => navigate("/")}>
-        <GoBackIcon />
-      </Button>
-      <BookmarkList />
-    </>
+    <PageContainer>
+      <Paper
+        width="100%"
+        flex="1"
+        bodyAlign="flex-start"
+        paperHeader={
+          <>
+            <PaperTitle>
+              Bookmark{" "}
+              <Button onClick={() => navigate("/")} width="35px" height="35px">
+                <GoBackIcon />
+              </Button>
+            </PaperTitle>
+          </>
+        }
+      >
+        <BookmarkList />
+      </Paper>
+    </PageContainer>
   );
 }
 
