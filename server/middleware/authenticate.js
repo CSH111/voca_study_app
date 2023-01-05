784f7134d228ca80040a37e0f1d5cf235a101b2c
CSH111 <seongho6022@naver.com>
2023-01-05 13:54:53 +0900

    feat: 로그인 에러시 에러네임 client 전송

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -11,12 +11,12 @@ function authenticate(req, res, next) {
     .exec()
     .then((doc) => {
       if (!doc) {
-        res.status(400).json({ msg: "존재하지 않는 아이디입니다." });
+        res.status(400).json({ msg: "존재하지 않는 아이디입니다.", errName: "noEmail" });
         return;
       }
       bcrypt.compare(temp.pw, doc.pw, (err, result) => {
         if (!result) {
-          res.status(400).json({ msg: "비밀번호가 다릅니다." });
+          res.status(400).json({ msg: "비밀번호가 다릅니다.", errName: "wrongPw" });
           return;
         }
         // 미들웨어에서 값을 next로 전달하고 싶을때 req.객체생성
