056bc8dd8af7885d4d7f0c67fc63e37959f1a556
CSH111 <seongho6022@naver.com>
2024-05-13 01:44:12 +0900

    서버 url 환경변수 적용

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -5,6 +5,5 @@ export { default as wordbookActionType } from "./wordbookActionType";
 
 export const MODAL_FADE_TIME = 100;
 export const INFO_URL = "https://github.com/CSH111/voca_study_app";
-export const SERVER_URL =
-  "https://port-0-voca-study-app-server-sop272gld48lazw.gksl2.cloudtype.app";
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 export const LOCAL_TEST_SERVER_URL = "http://localhost:5000";
